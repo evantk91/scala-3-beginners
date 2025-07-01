2b1787b3e7d14a76ef1df11b2bb6da55a3933098
Evan Greer <evan.greer@evan.greer>
2025-07-01 15:15:15 -0600

    anonymous functions lesson

---
+++ src/main/scala/com/rockthejvm/part3fp/AnonymousFunctions.scala
@@ -0,0 +1,42 @@
+package com.rockthejvm.part3fp
+
+object AnonymousFunctions {
+
+  // instances of FunctionN
+  val doubler: Int => Int = new Function1[Int, Int] {
+    override def apply(x: Int) = x * 2
+  }
+
+  // lambdas = anonymous function instances
+  val doubler_v2: Int => Int = (x: Int) => x * 2 // identical
+  val adder: (Int, Int) => Int = (x: Int, y: Int) => x + y // new Function2[Int, Int, Int] { override def apply...}
+
+  // zero arg function
+  val justDoSomething: () => Int = () => 45
+  val anInvocation = justDoSomething()
+
+  // alt syntax with curly braces
+  val stringToInt = { (str: String) =>
+    // implementation: code block
+    str.toInt
+  }
+
+  // type inference
+  val doubler_v3: Int => Int = x => x * 2 // type inferred by complier
+  val adder_v2: (Int, Int) => Int = (x, y) => x + y
+
+  // shortest lambdas
+  val doubler_v4: Int => Int = _ * 2 // x => x * 2, first argument of this function
+  val adder_v3: (Int, Int) => Int = _ + _ // (x, y) => x + y
+
+  /*
+   * Exercises
+   * 1. Replace all FunctionN instantiations with lambdas in LList
+   * 2. Rewrite the "special" adder from WhatsAFunction using Lambdas
+   */
+
+  def main(args: Array[String]): Unit = {
+    println(justDoSomething)
+    println(justDoSomething())
+  }
+}
